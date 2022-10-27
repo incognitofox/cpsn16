ec4afe19a81d562bc2b5c0dd273029afbeafebc6
Will <wywang@Will.localdomain>
2022-10-27 02:31:03 -0500

    school info

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,48 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Outlet, Link, useLocation } from "react-router-dom";
 import Papa from "papaparse"
 import logo from '../logo.svg'
 
 const allowedExtensions = ["csv"];
-const csv = Papa.parse("schools.csv", { header: true })
 
 const Home = () => {
     const state  = useLocation();
-    console.log(state)
+
+ 	const [data, setData] = useState([]);
+    const [error, setError] = useState("");
+	const [file, setFile] = useState("");
+	const [rows, setRows] = useState([])
+    	
+    var school = state['state']['school']
+
+	useEffect(() => {
+		async function getData() {
+		  const response = await fetch('/data/schools.csv')
+		  const reader = response.body.getReader()
+		  const result = await reader.read() // raw array
+		  const decoder = new TextDecoder('utf-8')
+		  const csv = decoder.decode(result.value) // the csv text
+		  const results = Papa.parse(csv, { header: true }) // object with { data, errors, meta }
+		  const rows = results.data // array of objects
+		  console.log(rows)
+		  setRows(rows)
+    	}
+	 	getData()	
+	}, [])
+
+    var row = rows[0]
+
+    for (var i = 0; i < rows.length; i ++) {
+        if (rows[i]['name'] == school)
+            row = rows[i]
+    }
+
+    console.log(row)
     return(
     <div className="App-header">
-        <p>{state['state']['school']}</p>
+        <p>{school}</p>
+        <a href={row['url']}>Link</a>
+        <p>{row['phone']}</p>
     </div>
 
     );
