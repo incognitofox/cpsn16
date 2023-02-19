9a6f0a3af1285ecba95e4508eb999b12aa41f5d0
Son Dam <dson@uchicago.edu>
2023-02-19 17:00:36 -0600

    Changes to Academic Resources

--- src/pages/AcademicResources.js
+++ src/pages/AcademicResources.js
@@ -4,7 +4,7 @@ const AcademicResources = () => {
 		<div>
 			<h1> Academic Resources </h1>
 			<section class = 'academicresources-container'>
-				<div class = "adacemicresources-one">
+				<div class = "academicresources-one">
 					<h2 class = "academicresources-page"> Free Online Resources</h2>
 					<div class = "academicresources-body">
 						<p> mimimi 
@@ -15,10 +15,33 @@ const AcademicResources = () => {
 
 				<hr class="hr-line"></hr>
 
-				<div class = "adacemicresources-two">
+				<div class = "academicresources-two">
 					<h2 class = "academicresources-page"> Nearby Events</h2>
 					<div class = "academicresources-body">
 						<p> mimimi
+						</p>
+					</div>
+				</div>
+				
+				<hr class="hr-line"></hr>
+
+				<div class = "academicresources-three">
+					<h2 class = "academicresources-page"> Summer Academic Enrichment Experiences</h2>
+					<div class = "academicresources-body">
+						<p>
+							<br /><a href="https://cps.edu/campaigns/summer-programs"> CPS Summer Programs</a> 
+							<br /><a href="https://www.chicagodebates.org/cdsi/"> Chicago Debates Summer Institute</a>
+							<br /><a href="https://mathcirclesofchicago.org/summer/"> Math Circles of Chicago</a> 
+							<br /><a href="https://www.cps.edu/academics/academic-enrichment-opportunities/summer-reading/"> CPS Summer Book Club</a>
+						</p>
+					</div>
+				</div>
+
+				<div class = "academicresources-four">
+					<h2 class = "academicresources-page"> Research Projects</h2>
+					<div class = "academicresources-body">
+						<p>
+							mimimi
 						</p>
 					</div>
 				</div>
