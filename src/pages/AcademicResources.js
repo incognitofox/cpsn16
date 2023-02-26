972e21e7078fe035cd27a094df413fc7f6412c0d
Son Dam <dson@uchicago.edu>
2023-02-26 16:59:52 -0600

    edited Academic Resources

--- src/pages/AcademicResources.js
+++ src/pages/AcademicResources.js
@@ -31,17 +31,35 @@ const AcademicResources = () => {
 						<p>
 							<br /><a href="https://cps.edu/campaigns/summer-programs"> CPS Summer Programs</a> 
 							<br /><a href="https://www.chicagodebates.org/cdsi/"> Chicago Debates Summer Institute</a>
-							<br /><a href="https://mathcirclesofchicago.org/summer/"> Math Circles of Chicago</a> 
+							<br /><a href="https://mathcirclesofchicago.org/summer/"> Math Circles of Chicago Summer Camp</a> 
 							<br /><a href="https://www.cps.edu/academics/academic-enrichment-opportunities/summer-reading/"> CPS Summer Book Club</a>
+							<br /><a href="www.onesummerchicago.org">One Summer Chicago</a>
+							<br /><a href="https://www.chicagoyouthcenters.org/outofschool-time-ost"> Out-of-School Time at CYC</a>
+							<br /><a href="https://cpschess.org/index.php/summer-camp/"> Chess Summer Camp</a>
 						</p>
 					</div>
 				</div>
 
 				<div class = "academicresources-four">
+					<h2 class = "academicresources-page"> Year-Round Academic Enrichment Experiences</h2>
+					<div class = "academicresources-body">
+						<p>
+							<br /><a href="https://resources.chicagodebates.org"> Chicago Debate League</a>
+							<br /><a href="https://www.cps.edu/sites/cpsarts/districtwide-arts-programs/student-programs/re-alize/"> RE:ALIZE (Premier Early College Arts Program)</a>
+							<br /><a href="https://www.cps.edu/sites/cpsarts/districtwide-arts-programs/student-programs/all-city-performing-arts/"> All-City Performing Arts</a>
+							<br /><a href="https://mathcirclesofchicago.org"> Math Circles of Chicago</a>
+							<br /><a href="https:www.cpsscifair.org/Main/resources.html"> Science Fair</a>
+							<br /> <a href="cpschess.org"> Chess Program</a>
+
+						</p>
+					</div>
+				</div>
+
+				<div class = "academicresources-five">
 					<h2 class = "academicresources-page"> Research Projects</h2>
 					<div class = "academicresources-body">
 						<p>
-							mimimi
+							<br /><a href="https://cps.edu/sites/districtwide-arts-programs/student-programs/arts-student-voice-committee/"> Arts Student Voice Committee</a>
 						</p>
 					</div>
 				</div>
